065c2e569e2ea461b65e7847bad36e8d993a61fc
Andrey Meshkov <am@adguard.com>
2022-03-10 23:29:38 +0300

    fix links

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            AdGuard Knowledgebase - 🔰
+            AdGuard DNS Knowledgebase - 🔰
           </Link>
         </div>
       </div>
