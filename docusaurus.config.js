0ba8f689b3297d92e45781e0376eac9eb6446237
Ildar Kamalov <ik@adguard.com>
2024-08-26 13:12:58 +0300

    AGDNS-2309 fix build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -237,7 +237,21 @@ module.exports = {
       },
     ],
   ],
-  plugins: [
-    '@docusaurus/plugin-ideal-image',
-  ],
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('swc-loader'),
+      options: {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          target: 'es2017',
+        },
+        module: {
+          type: isServer ? 'commonjs' : 'es6',
+        },
+      },
+    }),
+  },
 };
