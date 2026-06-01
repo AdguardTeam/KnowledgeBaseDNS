25681343ca27067c7b69b254a8fdb297ef9ad650
Ildar Kamalov <ik@adguard.com>
2026-06-01 15:42:21 +0300

    review fix

--- src/theme/Root/index.js
+++ src/theme/Root/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Root from '@theme-original/Root';
-import useDocusaurusContext from '@docusaurus/core/lib/client/exports/useDocusaurusContext';
-import { translate } from '@docusaurus/core/lib/client/exports/Translate';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { translate } from '@docusaurus/Translate';
 import { Context as DocusaurusContext } from '@docusaurus/core/lib/client/docusaurusContext';
 
 export default function RootWrapper({ children }) {
@@ -10,21 +10,23 @@ export default function RootWrapper({ children }) {
   const translatedTitle = translate({
     id: 'siteConfig.title',
     message: contextValue.siteConfig.title,
+    description: 'The site title used in the browser tab and metadata',
   });
 
   const translatedTagline = translate({
     id: 'siteConfig.tagline',
     message: contextValue.siteConfig.tagline,
+    description: 'The site tagline used in meta description',
   });
 
-  const modifiedContext = {
+  const modifiedContext = useMemo(() => ({
     ...contextValue,
     siteConfig: {
       ...contextValue.siteConfig,
       title: translatedTitle,
       tagline: translatedTagline,
     },
-  };
+  }), [contextValue, translatedTitle, translatedTagline]);
 
   return (
     <DocusaurusContext.Provider value={modifiedContext}>
